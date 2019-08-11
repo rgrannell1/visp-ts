30ea20437999ac7cb063300c8d67d151d118e4fb
rgrannell1 <r.grannell2@gmail.com>
2019-08-11 23:18:05 +0100

    added boolean tests

--- tests/parser.ts
+++ tests/parser.ts
@@ -3,7 +3,10 @@ import testing from '@rgrannell/testing'
 import * as parser from '../src/parser'
 import * as PC from '../src/pc'
 import {
-  ParseSource, ParseResult, isParseSuccess
+  ParseSource,
+  ParseResult,
+  isParseSuccess,
+  Parser
 } from '../src/types'
 
 interface ExpectedParse {
@@ -18,13 +21,25 @@ interface ExpectedParse {
 
 type ParseTest = [string, ExpectedParse]
 
+const expectedParse = (data: string, rest: string, lineNumber: number): ExpectedParse => {
+  return {
+    data: { source: data },
+    rest: { source: rest, lineNumber }
+  }
+}
+
 const cases = {} as Record<string, Array<ParseTest>>
 
 cases.comment = [
-  ['; comment\n', {
-    data: { source: '; comment\n' },
-    rest: { source: '', lineNumber: 2 }
-  }]
+  [';\n', expectedParse(';\n', '', 2)],
+  ['; comment\n', expectedParse('; comment\n', '', 2)],
+  ['; comment\nmore text', expectedParse('; comment\n', 'more text', 2)],
+]
+
+cases.boolean = [
+  ['#t', expectedParse('#t', '', 1)],
+  ['#f', expectedParse('#f', '', 1)],
+  ['#fmore text', expectedParse('#f', 'more text', 1)],
 ]
 
 const expectations = {
@@ -36,24 +51,28 @@ const expectations = {
     isParseSuccess(res) && res.rest.lineNumber === expected.rest.lineNumber
 }
 
-const createCases = (cases:Array<ParseTest>) => {
+const createCases = (cases: Array<ParseTest>, parser: Parser) => {
   return function* (): IterableIterator<any> {
     for (const [source, expected] of cases) {
-      const result = parser.comment(PC.input(source))
+      const result = parser(PC.input(source))
       yield [result, expected]
     }
   }
 }
 
-const hypotheses = {} as Record<string, Object>
+const hypotheses = {
+  comment: testing.hypothesis('comments parse successfully')
+    .cases(createCases(cases.comment, parser.comment))
+    .always(expectations.hasSource)
+    .always(expectations.hasRest)
+    .always(expectations.hasLineNumber),
+  boolean: testing.hypothesis('boolean parse successfully')
+    .cases(createCases(cases.boolean, parser.boolean))
+    .always(expectations.hasSource)
+    .always(expectations.hasRest)
+    .always(expectations.hasLineNumber),
+}
 
-hypotheses.comment = testing.hypothesis('comments parse successfully')
-  .cases(createCases(cases.comment))
-  .always(expectations.hasSource)
-  .always(expectations.hasRest)
-  .always(expectations.hasLineNumber)
 
-const theory = testing.theory({ description: 'Establish parsers work as expected' })
+export default testing.theory({ description: 'Establish parsers work as expected' })
   .givenAll(hypotheses)
-
-export default theory
